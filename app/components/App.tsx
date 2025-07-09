511c0880040933ae9b7ddcfbc4cf02c87629ecde
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-07-09 16:29:26 -0400

    fix status bar

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,15 +1,11 @@
 import { Navigation, linking } from '@/app/components/Navigation';
 import { Text, StatusBar } from 'react-native';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { colors } from '@/app/style';
 
 export function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar backgroundColor={colors.primaryLight} /> {/* TODO */}
-      <SafeAreaView style={{ flex: 1 }}>
-        <Navigation linking={linking} fallback={<Text>Loading...</Text>} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Navigation linking={linking} fallback={<Text>Loading...</Text>} />
+    </>
   );
 }
