3bd6526ac38b398deba38e15588d8f49bbeaec70
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-07-10 20:43:32 -0400

    delete everything

--- app/hooks/useNavigation.ts
+++
@@ -1,9 +0,0 @@
-import type { StackParamList } from '@/app/components/Navigation';
-
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
-import { useNavigation } from '@react-navigation/native';
-
-export const useTypedNavigation = useNavigation<
-  NativeStackNavigationProp<StackParamList>
->;
--- app/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare type AppRoute = 'Home' | 'About';
-
-type RouteParams = { from?: string } | undefined;
-
-declare type RootStackParamList = {
-  [route in AppRoute]: RouteParams;
-};
